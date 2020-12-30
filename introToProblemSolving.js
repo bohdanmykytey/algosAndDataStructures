5e529d08b4cbf506ff1f1c8a1d48956333abc58d
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2020-12-30 17:51:24 -0500

    pseudocoding

---
+++ introToProblemSolving.js
@@ -0,0 +1,56 @@
+//CONCRETE GAMEPLAN TO BUILDING BETTER ALGOS.
+
+//Undestranding the problem
+
+/*
+1) Restate the problem in your own words
+2) What are the inputs
+3) What outputs should be coming out of the function
+4) Can the outputs be determined from the inputs?
+5) How do you properly label the important bids of data.
+*/
+
+//write a function that takes in two numbers and returns their sum
+/*
+1) Restate the problem in your own words
+    - implement addition
+2) What are the inputs
+    - two ints, but can it work with a different number of inputs
+3) What outputs should be coming out of the function
+    - one int, a sum of the int inputs
+4) Can the outputs be determined from the inputs?
+    - yes.
+5) How do you properly label the important bids of data.
+*/
+
+//Problem Solving
+
+/*
+Expmlore examples
+1) Start with simple examples
+2) Progress to more complex examples
+3) Explore examples with empty inputs
+4) Explore examples with invalid inputs
+*/
+
+//write a function that takes in a string and returns counts of each char in the str
+
+//Break It Down
+//the interviewr told us that we are keeping track of the numbers and only lower case letters
+
+/*
+function charCount()    {
+    //make object to return at end
+    //loop over string
+    //return object 
+}
+*/
+
+function charCount(str)    {
+    //make object to return at end
+    //loop over string, for each char.
+        //if the number/letter AND is a key in the object, add it to 1
+        //if number/letter AND not add it and set value to 1
+        //if char is something other than alphanumeric dont do anything
+    //return object 
+}
