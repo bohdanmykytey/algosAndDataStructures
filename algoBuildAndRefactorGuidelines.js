610ad63727e1a83dcafbfc46cca278f668d0b0bd
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2021-01-03 13:16:48 -0500

    making sure everything is up to date

--- algoBuildAndRefactorGuidelines.js
+++ algoBuildAndRefactorGuidelines.js
@@ -94,15 +94,10 @@ function charCount(str) {
     for (let char of str) {
         char = char.toLowerCase()
         if (/[a-z0-9]/.test(char)) {
-            if (result[char] > 0) {
-                result[char]++
-            } else {
-                result[char] = 1;
-            }
+            result[char] = ++result[char] || 1;
         }
-
     }
     return result;
 }
 
-console.log(charCount('Hi there'))
+console.log(charCount('Hi there, My name is Bohdan!!!'))
