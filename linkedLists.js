53f6ed6b7a0a35e20b25a1f5e44a3d1f7c4685f0
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2021-01-14 00:14:11 -0500

    inserting at given index

--- linkedLists.js
+++ linkedLists.js
@@ -1,4 +1,4 @@
-//PRIMITIVE EXAMPLE
+// PRIMITIVE EXAMPLE
 
 // const node1 = {
 //     data: 100
@@ -10,29 +10,83 @@
 
 // node1.next = node2;
 
-// console.log(node1)
-
 //IMPLEMENTATION 
 
 class Node {
-    constructor(data, next = null)  {
+    constructor(data, next = null) {
         this.data = data;
         this.next = next;
     }
 }
 
+const node1 = new Node(100)
+
 class LinkedList {
-    constructor()   {
+    constructor() {
         this.head = null;
         this.size = 0;
     }
+
+    //insert head
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+        this.size++;
+    }
+
+    //print list data
+    printListData() {
+        let current = this.head;
+
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+
+    }
+
+    //inster last node
+    insertLast(data) {
+        let node = new Node(data);
+        let current;
+
+        //if empty make it head
+        if (!this.head) {
+            this.head = node;
+        } else {
+            current = this.head;
+
+            while (current.next) {
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+
+        this.size++;
+    }
+
+    //at an index
+    inserAt(data, index) {
+        //if index is out of range
+        if (index > 0 && index > this.size) {
+            return 'Insert Index is > this.size'
+        }
+
+        //if first index then set to head
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+
+        const node = new Node(data);
+        let current, previous;
+
+        current = this.head;
+    }
 }
 
-//insert first node 
 
-//inster last node
 
-//at an index
 
 //get at a certain index
 
@@ -40,5 +94,13 @@ class LinkedList {
 
 //clear list
 
-//print list data
 
+
+const linkedList = new LinkedList();
+
+linkedList.insertFirst(100)
+linkedList.insertFirst(200)
+linkedList.insertFirst(300)
+linkedList.insertLast(400)
+
+linkedList.printListData()
