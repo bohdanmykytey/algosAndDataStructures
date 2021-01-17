ba7d6eea70f3b1d480a459a73f9d126e5bdc42d4
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2021-01-17 15:35:06 -0500

    rewriting for loop recursively

---
+++ Recursion/baseCase.js
@@ -0,0 +1,34 @@
+/*
+    When does recursion stop?
+         - base cases end recursion
+         - OR when a different input is passed in, in other
+           words the part of an input changes as it parses the
+           entire input
+*/
+
+/*
+    Implement a function that counts down from 5.
+*/
+
+//regual for loop
+function countDown(start) {
+    for(let i = start; i > 0; i--)  {
+        console.log(i)
+    }
+    console.log('all done')
+}
+
+// countDown(5)
+
+//recursive solution
+function countDownRecursively(num)  {
+    if(num <= 0) {
+        console.log('all done recursively')
+        return; //if this return statement wasnt here it would keep decrementing for ever
+    }
+    console.log(num);
+    num--;
+    countDownRecursively(num)
+}
+
+countDownRecursively(5)
