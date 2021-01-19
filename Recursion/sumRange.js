3e5c97e57da7f3419485cadd758a600b80e6cba8
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2021-01-19 15:03:43 -0500

    sumRange refactored

--- Recursion/sumRange.js
+++ Recursion/sumRange.js
@@ -1,6 +1,11 @@
-function sumRange(num) {
-    if(num === 1) return 1;//base case that breaks the recursive loop
-    return num + sumRange(num - 1)//recursive call
+// function sumRange(num) {
+//     if(num === 1) return 1;//base case that breaks the recursive loop
+//     return num + sumRange(num - 1)//recursive call
+// }
+
+//refactroed with one line
+function sumRange(num)  {
+    return num === 1 ? 1 : num + sumRange(num - 1)
 }
 
 console.log(sumRange(4))
