089545e2ede2a1ecf316edc12a0b88bc00d3fb8f
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2021-01-19 15:43:40 -0500

    recusrsion finalized

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -17,10 +17,11 @@ function factorial(num) {
 // console.log(factorial(4))
 
 //recursive solution
-//undesrtand the base case and recursive call
+//undesrtand the base case and recursive call - witout them you will hit a stack overflow
 
 function factorialRecursive(num) {
-    return num === 1 ? 1 : num * factorialRecursive(num - 1)
+    return num === 1 ? 1 : num * factorialRecursive(num - 1);
 }
 
-console.log(factorialRecursive(3))
+console.log(factorialRecursive(5));
+//returns 5 * 4 * 3 * 2 * 1 = 120
