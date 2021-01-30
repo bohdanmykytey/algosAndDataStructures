01056f5a335890468bae2c1ce29a0a64ea296786
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2021-01-30 14:36:09 -0500

    selection sorting algo

---
+++ Sotring Algos/selectionSort.js
@@ -0,0 +1,35 @@
+/*
+    Selection Sort:
+
+    Similar to bubble sort, but instead of first placing the large values into sorted 
+    positions, it places the small values into sorted positions.
+
+    Building a bubble sort algo:
+
+    - Store the first elem as the smallest value so far
+    - Keep comparing the value to the next value until you find a smaller num
+    - If a smaller val is found designate that as the new min and continue
+      till the end of the array
+    - If the min is not the val(index) then you swap the two
+    - Repeat process starting with the next item
+*/
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let smallest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[smallest]) {
+        smallest = j;
+      }
+      //swap! but only if i !== smallest
+      if (i !== smallest) {
+        let temp = arr[i];
+        arr[i] = arr[smallest];
+        arr[smallest] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort([0, 2, 34, 22, 10, 19, 17]));
